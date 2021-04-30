e66bdd2258e67ea99fda7104528d2b7aa8e5ea2f
Roger Rajaratnam <roger@sourcier.uk>
2021-04-30 12:30:04 +0100

    feat: 💄 Add app header and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = props => {
+  return (
+    <footer class="info">
+      <p>Double-click to edit a todo</p>
+      <p>Template by <a href="http://sindresorhus.com">Sindre Sorhus</a></p>
+      <p>Created by <a href="http://sourcier.uk">Roger Rajaratnam</a></p>
+      <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = props => {
+  return (
+    <header class="header">
+      <h1>todos</h1>
+    </header>
+  )
+}
+
+export default Header;
