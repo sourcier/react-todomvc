57060bb6b3e00435f3e3b136b28b54a3f86b2eb6
Roger Rajaratnam <roger@sourcier.uk>
2021-04-30 16:24:26 +0100

    feat: ✨ Add Recoil logger for development

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import {
 import 'todomvc-common/base.css'
 import 'todomvc-app-css/index.css'
 
+import DebugObserver from './libs/recoil/DebugObserver'
 import Home from './Home';
 
 const App = () => (
   <RecoilRoot>
+    {process.env.NODE_ENV === 'development' && <DebugObserver />}
     <Switch>
       <Route exact path="/" component={Home} />
     </Switch>
