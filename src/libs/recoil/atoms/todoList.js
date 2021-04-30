6710588e22e97c2ecfb14eb9d0b372eeab92f483
Roger Rajaratnam <roger@sourcier.uk>
2021-04-30 23:29:20 +0100

    fix: 🐛 Add missing todo atoms

--- src/libs/recoil/atoms/todoList.js
+++ src/libs/recoil/atoms/todoList.js
@@ -1,8 +1,43 @@
-import { atom } from 'recoil'
+import { atom, selector } from 'recoil'
 
 export const todoListState = atom({
   key: 'todoListState',
   default: [],
 })
+
+export const todoListStatsState = selector({
+  key: 'todoListStatsState',
+  get: ({ get }) => {
+    const todoList = get(todoListState)
+
+    return {
+      total: todoList.length,
+      totalCompleted: todoList.filter(({ isComplete }) => isComplete).length,
+      totalUncompleted: todoList.filter(({ isComplete }) => !isComplete).length,
+    }
+  }
+})
+
+export const todoListFilterState = atom({
+  key: 'todoListFilterState',
+  default: 'All',
+});
+
+export const filteredTodoListState = selector({
+  key: 'filteredTodoListState',
+  get: ({ get }) => {
+    const filter = get(todoListFilterState);
+    const todoList = get(todoListState);
+
+    switch (filter) {
+      case 'Completed':
+        return todoList.filter(({ isComplete }) => isComplete)
+      case 'Active':
+        return todoList.filter(({ isComplete }) => !isComplete)
+      default:
+        return todoList
+    }
+  },
+});
 
 export default todoListState
